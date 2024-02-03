b41f90538c792ae25c287c961a32aa929a32d698
Poleszak <poleszak@protonmail.com>
2024-02-03 14:23:01 +0100

    Feature - implement get car group dto by id

--- car-management-service/src/main/java/com/poleszak/carmanagementservice/controller/query/CarGroupQueryController.java
+++ car-management-service/src/main/java/com/poleszak/carmanagementservice/controller/query/CarGroupQueryController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,11 @@ public class CarGroupQueryController {
     public ResponseEntity<List<CarGroupDTO>> getAllCarGroupDTOs() {
 
         return ResponseEntity.ok().body(carGroupQueryService.getListOfAllCarGroupDTOs());
+    }
+
+    @GetMapping("/{carGroupId}")
+    public ResponseEntity<CarGroupDTO> getCarGroupDTOById(@PathVariable("carGroupId") Long carGroupId) {
+
+        return ResponseEntity.ok().body(carGroupQueryService.getCarGroupDTOById(carGroupId));
     }
 }
