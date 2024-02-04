ed2d49ec8353ee4fb216e4305c20034c835688c5
Poleszak <poleszak@protonmail.com>
2024-02-04 10:55:11 +0100

    Fix - clean code

--- car-management-service/src/main/java/com/poleszak/carmanagementservice/service/tools/mapper/CarMapper.java
+++ car-management-service/src/main/java/com/poleszak/carmanagementservice/service/tools/mapper/CarMapper.java
@@ -3,9 +3,10 @@ package com.poleszak.carmanagementservice.service.tools.mapper;
 import com.poleszak.carmanagementservice.controller.command.request.CarCreationRequest;
 import com.poleszak.carmanagementservice.model.Car;
 import org.mapstruct.Mapper;
-import org.mapstruct.NullValueCheckStrategy;
 
-@Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
+
+@Mapper(componentModel = "spring", nullValueCheckStrategy = ALWAYS)
 public interface CarMapper {
 
     Car carCreationRequestToCar(CarCreationRequest carCreationRequest);
