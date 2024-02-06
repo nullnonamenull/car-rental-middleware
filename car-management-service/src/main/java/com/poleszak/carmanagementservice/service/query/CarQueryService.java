cc64182c716c6b28497c3e8eb1096e18f16f4bda
Poleszak <poleszak@protonmail.com>
2024-02-06 23:16:44 +0100

    Impl - add repository to service

--- car-management-service/src/main/java/com/poleszak/carmanagementservice/service/query/CarQueryService.java
+++ car-management-service/src/main/java/com/poleszak/carmanagementservice/service/query/CarQueryService.java
@@ -1,9 +1,12 @@
 package com.poleszak.carmanagementservice.service.query;
 
+import com.poleszak.carmanagementservice.repository.CarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CarQueryService {
+
+    private final CarRepository carRepository;
 }
