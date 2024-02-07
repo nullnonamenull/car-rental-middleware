2192ebb00138377caf4a0da11f0b49b07b002e5c
Poleszak <poleszak@protonmail.com>
2024-02-07 21:34:28 +0100

    Impl - getAllCarDTOs

--- car-management-service/src/main/java/com/poleszak/carmanagementservice/service/query/CarQueryService.java
+++ car-management-service/src/main/java/com/poleszak/carmanagementservice/service/query/CarQueryService.java
@@ -1,12 +1,24 @@
 package com.poleszak.carmanagementservice.service.query;
 
+import com.poleszak.carmanagementservice.model.dto.CarDTO;
 import com.poleszak.carmanagementservice.repository.CarRepository;
+import com.poleszak.carmanagementservice.service.tools.mapper.CarMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CarQueryService {
 
     private final CarRepository carRepository;
+    private final CarMapper carMapper;
+
+    public List<CarDTO> getAllCarDTOs() {
+        return carRepository.findAll()
+                .stream()
+                .map(carMapper::carToCarDTO)
+                .toList();
+    }
 }
--- car-management-service/src/main/java/com/poleszak/carmanagementservice/service/tools/mapper/CarMapper.java
+++ car-management-service/src/main/java/com/poleszak/carmanagementservice/service/tools/mapper/CarMapper.java
@@ -2,6 +2,7 @@ package com.poleszak.carmanagementservice.service.tools.mapper;
 
 import com.poleszak.carmanagementservice.controller.command.request.CarCreationRequest;
 import com.poleszak.carmanagementservice.model.Car;
+import com.poleszak.carmanagementservice.model.dto.CarDTO;
 import org.mapstruct.Mapper;
 
 import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
@@ -10,5 +11,7 @@ import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
 public interface CarMapper {
 
     Car carCreationRequestToCar(CarCreationRequest carCreationRequest);
+
+    CarDTO carToCarDTO(Car car);
 
 }
